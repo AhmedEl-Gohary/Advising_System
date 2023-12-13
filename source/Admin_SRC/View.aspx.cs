28050f5126dca7b0d31646558d2b6245ed3b50f0
AhmedYTawfik <ahmedyasser55577@gmail.com>
2023-12-13 17:24:01 +0200

    going to views

--- source/Admin_SRC/View.aspx.cs
+++ source/Admin_SRC/View.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -13,10 +16,61 @@ namespace source.Admin_SRC
         {
 
         }
+        private void BindGridView(string viewName)
+        {
+            string connectionString = WebConfigurationManager.ConnectionStrings["Advising_System"].ToString();
+            string query = $"SELECT * FROM {viewName}";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
 
-		protected void Button1_Click(object sender, EventArgs e)
-		{
+                // Bind data to the GridView
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+            }
+        }
+        private string GetViewName(string selectedValue)
+        {
+            switch (selectedValue)
+            {
+                case "1":
+                    return "Student_Payment";
+                case "2":
+                    return "adasdas";
+                case "3":
+                    return "Semster_offered_Courses";
+                case "4":
+                    return "Students_Courses_transcript";
+                case "5":
+                    return "Student_Payment";
+                case "6":
+                    return "Semster_offered_Courses";
+                case "7":
+                    return "Students_Courses_transcript";
+                case "8":
+                    return "Student_Payment";
+                case "9":
+                    return "Semster_offered_Courses";
+                case "10":
+                    return "Semster_offered_Courses";
+                default:
+                    return "";
+            }
+        }
 
-		}
+        protected void Button1_Click(object sender, EventArgs e)
+        { 
+            string selectedValue = list.SelectedValue;
+            string viewName = GetViewName(selectedValue);
+            if (viewName == "")
+            {
+                msg.Text = "Balabizak Ya user";
+                return;
+            }
+            msg.Text = " ";
+            BindGridView(viewName);
+        }
 	}
 }

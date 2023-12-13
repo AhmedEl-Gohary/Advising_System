e8b3c72dc2abca3b65e203832446eb8a87a5beac
Ahmed Hussein <ahmedhusseinali107@gmail.com>
2023-12-13 21:34:13 +0200

    Checking and Fornt-End is Remaining

--- source/Student_SRC/Grad_Plan/Plan.aspx.cs
+++ source/Student_SRC/Grad_Plan/Plan.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +12,34 @@ namespace source.Student_SRC.Grad_Plan
 {
     public partial class Plan : System.Web.UI.Page
     {
+        int studentId = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session == null || Session["studentID"] == null)
+            {
+                Response.Redirect("../Error_Page.aspx");
+            }
+            else
+            {
+                studentId = int.Parse(Session["studentID"].ToString());
+            }
+            string connstr = WebConfigurationManager.ConnectionStrings["Advising_System"].ToString();
+            using (SqlConnection conn = new SqlConnection(connstr))
+            {
+                conn.Open();
+                string table = $"dbo.FN_StudentViewGP({studentId})";
+                string query = $"SELECT * FROM {table}";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+                if(dataTable.Rows.Count == 0)
+                {
+                    Label1.Text = "You don't have Graduation Plan Yet!";
+                }
+                conn.Close();
+            }
         }
     }
 }
--- source/Student_SRC/Makeup_Exams/View.aspx
+++ source/Student_SRC/Makeup_Exams/View.aspx
@@ -9,6 +9,10 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+            <br />
+            <asp:Label ID="Label1" runat="server" Text=" "></asp:Label>
         </div>
     </form>
 </body>

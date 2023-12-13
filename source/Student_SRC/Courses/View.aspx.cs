fda7bfc2650765edd06bdcd3672a789f73d836c9
AhmedKamal18 <ahmedelbakly95@gmail.com>
2023-12-13 19:38:57 +0200

    added student view courses

--- source/Student_SRC/Courses/View.aspx.cs
+++ source/Student_SRC/Courses/View.aspx.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,9 +13,121 @@ namespace source.Student_SRC.Courses
 {
     public partial class View : System.Web.UI.Page
     {
+        int studentID;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session == null || Session["studentID"] == null)
+            {
+                Response.Redirect("../../Error_Page.aspx");
+            }
+            else
+            {
+                studentID = int.Parse(Session["studentID"].ToString());
+            }
+            string selectedValue = List.SelectedValue;
+            SemesterCode.Enabled = !(selectedValue == "-1" 
+                || selectedValue == "1" || selectedValue == "5");
+            msg.Text = " ";
+        }
 
+        private void BindGridView(string procName)
+        {
+            string connectionString = WebConfigurationManager.ConnectionStrings["Advising_System"].ToString();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command;
+                string query;
+                string CurrentSemesterCode = SemesterCode.Text;
+                if(procName == "Courses_Slots_Instructor")
+                {
+                    query = $"Select * From {procName}";
+                    command = new SqlCommand(query, connection);
+                }
+                else if (procName == "SemsterAvailableCourses")
+                {
+                    query = $"Select * From dbo.FN_{procName}(@semestercode)";
+                    command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@semestercode", CurrentSemesterCode);
+                }
+                else
+                {
+                    query = $"Procedures_{procName}";
+                    command = new SqlCommand(query, connection)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    command.Parameters.AddWithValue("@studentID", studentID);
+                    if(procName != "ViewMS")
+                    {
+                        command.Parameters.AddWithValue("@current_semester_code", CurrentSemesterCode);
+                    }
+                }
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                connection.Open();
+                adapter.Fill(dataTable);
+                connection.Close();
+                GridView1.DataSource = dataTable;
+                GridView1.DataBind();
+            }
+        }
+
+
+
+
+        private string GetProcName(string selectedValue)
+        {
+            switch (selectedValue)
+            {
+                case "1":
+                    return "Courses_Slots_Instructor";
+                case "2":
+                    return "SemsterAvailableCourses";
+                case "3":
+                    return "ViewRequiredCourses";
+                case "4":
+                    return "ViewOptionalCourse";
+                case "5":
+                    return "ViewMS";
+                default:
+                    return "";
+            }
+        }
+
+        private string GetMessage(string selectedValue)
+        {
+            switch (selectedValue)
+            {
+                case "1":
+                    return "No Courses Yet";
+                case "2":
+                    return "No Available Courses in this Semester";
+                case "3":
+                    return "No Required Courses in this Semester";
+                case "4":
+                    return "No Optional Courses in this Semester";
+                case "5":
+                    return "No Missing Courses";
+                default:
+                    return "";
+            }
+        }
+
+        protected void Show(object sender, EventArgs e)
+        {
+            msg.Text = " ";
+            string selectedValue = List.SelectedValue;
+            string procName = GetProcName(selectedValue);
+            if (procName == "")
+            {
+                msg.Text = "Please specify the type!";
+                return;
+            }
+            BindGridView(procName);
+            if (GridView1.Rows.Count == 0)
+            {
+                msg.Text = GetMessage(selectedValue);
+            }
         }
     }
 }
--- source/Student_SRC/Student_Register_Page.aspx.cs
+++ source/Student_SRC/Student_Register_Page.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -12,6 +15,49 @@ namespace source.Student_SRC
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Login(object sender, EventArgs e)
+        {
+            Response.Redirect("../Login_Page.aspx");
+        }
+
+        protected void Register(object sender, EventArgs e)
+        {
+            string firstName = fname.Text;
+            string lastName = lname.Text;
+            string facultyName = faculty.Text;
+            string majorName = major.Text;
+            string pass = password.Text;
+            string mail = email.Text;
+            int currSemester;
+            try
+            {
+                currSemester = int.Parse(semester.Text);
+            }
+            catch
+            {
+                msg.Text = "semester is a number indicating your current semester!";
+                return;
+            }
+            string connstr = WebConfigurationManager.ConnectionStrings["Advising_System"].ToString();
+            SqlConnection conn = new SqlConnection(connstr);
+            SqlCommand register = new SqlCommand("[Procedures_StudentRegistration]", conn);
+            register.CommandType = CommandType.StoredProcedure;
+            register.Parameters.AddWithValue("@first_name", firstName);
+            register.Parameters.AddWithValue("@last_name", lastName);
+            register.Parameters.AddWithValue("@password", pass);
+            register.Parameters.AddWithValue("@faculty", facultyName);
+            register.Parameters.AddWithValue("@email", mail);
+            register.Parameters.AddWithValue("@major", majorName);
+            register.Parameters.AddWithValue("@Semester", currSemester);
+            SqlParameter studentID = register.Parameters.Add("@Student_id", SqlDbType.Int);
+            //TODO continue the login
+            studentID.Direction = ParameterDirection.Output;
+            conn.Open();
+            register.ExecuteNonQuery();
+            registerSuccesslabel.Text = "Registered Succesfully! Your ID is " + studentID.Value.ToString();
+            conn.Close();
         }
     }
 }

143463564537d4689581155346f8f14a8b3ece10
Ahmed Hussein <ahmedhusseinali107@gmail.com>
2023-12-16 21:15:03 +0200

    Add styling to some web forms

--- source/Student_SRC/Financial/Installments.aspx.designer.cs
+++ source/Student_SRC/Financial/Installments.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace source.Student_SRC.Financial
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// Go_Back control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Go_Back;
+
+        /// <summary>
         /// GridView1 control.
         /// </summary>
         /// <remarks>
@@ -31,5 +40,14 @@ namespace source.Student_SRC.Financial
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
